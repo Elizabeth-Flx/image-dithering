eb8a924588b1577029212cb2fa6a3c16a2a01e83
Daniel Lochert <lochert@cip.ifi.lmu.de>
2023-06-16 22:48:30 +0200

    Added method that processes the full image

--- src/backend/DitheringAlgo.java
+++ src/backend/DitheringAlgo.java
@@ -2,6 +2,22 @@ package backend;
 
 public abstract class DitheringAlgo {
 
-    public abstract void processPixel(int[][][] image, int x, int y, int[] targetColor);
+    // Image format
+    //
+    //  +--------->
+    //  |       y
+    //  |
+    //  | x
+    //  v
+
+    public void processFullImage(int[][][] image, Palette palette) {
+        for (int x = 0; x < image.length; x++) {
+            for (int y = 0; y < image.length; y++) {
+                processPixel(image, x, y, palette);
+            }
+        }
+    }
+
+    public abstract void processPixel(int[][][] image, int x, int y, Palette palette);
 
 }
